2e7cbc309d40010337e89a13d044f55a9df9849f
Armaan C <armaanchandarana1@gmail.com>
2026-02-25 11:25:03 -0500

    SW serve index.html for all routes

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,19 +1,18 @@
 /* eslint-disable no-restricted-globals */
 
 import { clientsClaim } from "workbox-core";
-import { precacheAndRoute } from "workbox-precaching";
+import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { NavigationRoute } from "workbox-routing";
 
 clientsClaim();
 self.skipWaiting();
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-// 👇 Add this
-const handler = async ({ request }) => {
-  return caches.match('/index.html');
-};
+const handler = createHandlerBoundToURL("/index.html");
 
-const navigationRoute = new NavigationRoute(handler);
-registerRoute(navigationRoute);
+registerRoute(
+  ({ request, url }) =>
+    request.mode === "navigate",
+  handler
+);
