3ad3e09c2839b18286b0a3ebcb23ba697a22ac0b
Armaan C <armaanchandarana1@gmail.com>
2026-03-03 15:30:28 -0500

    meaningless change to force client update

--- server/Constants.js
+++ server/Constants.js
@@ -1,3 +1,4 @@
 // update in client too
 export const PRACTICE_EVENTS = []; //["2025week0", "2025onnew"];
 export const ATTENDING_EVENTS = ["2017oncmp", "2026week0"]; //, "2025ontor", "2025oncmp"];
+
